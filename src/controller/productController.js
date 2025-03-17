81ce5401cf86ea5276097bbb6b4b612f13c7be6b
Shanilka800@# <shanilka799@gmail.com>
2025-03-18 00:07:24 +0530

    create don productController getAllProduct funtion

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -5,13 +5,16 @@ const createProduct = async (req, res) => {
     const product = await productService.createProduct(req.body);
     res.status(200).json(product);
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    res.status(400).json({ error: error.message });
   }
 };
 
 const getAllProduct = async (req, res) => {
   try {
     const product = await productService.getAllProduct();
+    res.status(200).json(product);
+  } catch (eror) {
+    res.status(400).json({error: error.message});
   }
 }
 
