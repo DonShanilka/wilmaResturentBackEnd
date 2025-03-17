5d633fdaa6cd381dac76a86de0f8e6ef613925d4
Shanilka800@# <shanilka799@gmail.com>
2025-03-18 00:11:28 +0530

    update productRouter

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const productController = require('../controller/productController');
 
 router.post('/product/saveProducts', productController.createProduct);
+router.post('/product/getAllProduct', productController.getAllProduct);
 
 module.exports = router;
