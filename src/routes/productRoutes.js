5bf66ac24c09a5620d92a8b89781b17e240ddb36
Shanilka800@# <shanilka799@gmail.com>
2025-03-18 00:44:30 +0530

    update product routs

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -4,5 +4,6 @@ const productController = require('../controller/productController');
 
 router.post('/product/saveProducts', productController.createProduct);
 router.get('/product/getAllProducts', productController.getAllProduct);
+router.get('/product/getProductById', productController.getProductById);
 
 module.exports = router;
