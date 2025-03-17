b0ec4e9873c6945138649c10c3658caf9f7eaf20
Shanilka800@# <shanilka799@gmail.com>
2025-03-17 21:52:56 +0530

    connectMongoDb

--- index.js
+++ index.js
@@ -13,5 +13,11 @@ app.get('/', (req, res) => {
   res.send("Hello form Node API")
 });
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("✅ MongoDB Connected Successfully"))
+  .catch((err) => console.error("❌ MongoDB Connection Error:", err));
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
