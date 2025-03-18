077755682f9834292f8a63b7707c8a43e97cf17f
Shanilka800@# <shanilka799@gmail.com>
2025-03-18 23:00:10 +0530

    create deleteProduct funtion in productController

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -20,6 +20,17 @@ const updateProduct = async (req, res) => {
   }
 };
 
+const deleteProduct = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const product = await productService.deleteProduct(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
 const getAllProduct = async (req, res) => {
   try {
     const product = await productService.getAllProduct();
@@ -46,4 +57,5 @@ module.exports = {
   getAllProduct,
   getProductById,
   updateProduct,
+  deleteProduct
 };
