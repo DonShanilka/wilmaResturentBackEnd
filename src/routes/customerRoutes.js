9bc5538f0d96c10ab4eacaedcea7f6dc37dd3a28
Shanilka800@# <shanilka799@gmail.com>
2025-03-19 23:17:52 +0530

    create customerRouter

---
+++ src/routes/customerRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const customerController = require('../controller/CustomerController');
+
+router.post('/customer/saveCustomer', customerController.addCustomer);
+router.get('/customer/getAllCustomers', customerController.getAllCustomers);
+router.get('/customer/getCustomerById/:id', customerController.getCustomerById);
+router.put('/customer/updateCustomer/:id', customerController.updateCustomer);
+router.delete('/customer/deleteCustomer/:id', customerController.deleteCustomer);
+
+module.exports = router;
