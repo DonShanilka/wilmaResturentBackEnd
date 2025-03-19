8d8c7d43c3a4559631594fe5cacd0565e6755bc7
Shanilka800@# <shanilka799@gmail.com>
2025-03-19 23:07:58 +0530

    create updateCustomer in CustomerController

--- src/controller/CustomerController.js
+++ src/controller/CustomerController.js
@@ -8,3 +8,14 @@ const addCustomer = async (req, res) => {
     res.status(400).json({ error: error.message });
   }
 }
+
+const updateCustomer = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const customer = await CustomerService.updateCustomer(id, req.body);
+    res.status(200).json(customer);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
