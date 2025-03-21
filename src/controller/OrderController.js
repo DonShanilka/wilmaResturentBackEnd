fd73de48e74f84c51ffeec8fbb696bc0c5d03129
Shanilka800@# <shanilka799@gmail.com>
2025-03-21 20:05:53 +0530

    create getOrderById in orderController

--- src/controller/OrderController.js
+++ src/controller/OrderController.js
@@ -8,3 +8,23 @@ const placeOrder = async(req, res) => {
     res.status(400).json({ error: error.message });
   }
 }
+
+const getAllOrders = async(req, res) => {
+  try {
+    const orders = await orderService.getAllOrders();
+    res.status(200).jscon(orders);
+  } catch(error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+const getOrderById = async(req, res) => {
+  const {id} = req.params;
+
+  try {
+    const order = await orderService.getOrderById(id);
+    res.status(200).json(order);
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
+}
